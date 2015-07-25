9488057933335a8e0bd9551bb3fd64621f5fa487
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-07-25 15:40:42 +0530

    Fix the tests

--- test/test.js
+++ test/test.js
@@ -12,11 +12,19 @@ describe("json2sass", function() {
     it("should convert json to sass", function() {
         var result;
 
+        try {
+            fs.mkdirSync(__dirname + "/../dist");
+        } catch (err) {
+            if (err.code !== "EEXIST") {
+                throw err;
+            }
+        }
+
         fs.writeFileSync(__dirname + "/../dist/_data.scss", json2sass(data), "utf8");
 
         result = sass.renderSync({
-          file: __dirname + "/style.scss",
-          outputStyle: "compressed"
+            file: __dirname + "/style.scss",
+            outputStyle: "compressed"
         });
 
         assert.equal(result.css.toString(), fs.readFileSync(__dirname + "/compiled.scss", "utf8"));
